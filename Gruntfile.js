c7b201934eb65f8be04ce09bb7d21c49e108e386
jeroen.abrahams <jeroen.abrahams@onsophic.com>
2018-11-23 11:33:25 +0100

    first version

---
+++ Gruntfile.js
@@ -0,0 +1,66 @@
+/*
+ * grunt-angular-translate-auto-add-missing-keys
+ * https://github.com/jeroenabrahams/angular-translate-auto-add-missing-keys
+ *
+ * Copyright (c) 2018 jeroen.abrahams
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+      options: {
+        jshintrc: '.jshintrc',
+        reporterOutput: "",
+      }
+    },
+
+    // Before generating any new files, remove any previously-created files.
+    clean: {
+      tests: ['tmp']
+    },
+
+    // Configuration to be run (and then tested).
+    angular_translate_auto_add_missing_keys: {
+      default_options: {
+        options: {
+          baseLang: 'base',
+          dest: 'tmp/fixtures'
+        },
+        src: 'test/fixtures/*.json'
+      },
+
+    },
+
+    // Unit tests.
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  // Actually load this plugin's task(s).
+  grunt.loadTasks('tasks');
+
+  // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
+  // plugin's task(s), then test the result.
+  grunt.registerTask('test', ['clean', 'angular_translate_auto_add_missing_keys', 'nodeunit']);
+
+  // By default, lint and run all tests.
+  grunt.registerTask('default', ['jshint', 'test']);
+
+};
